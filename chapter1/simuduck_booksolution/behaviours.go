cbe9298a9bbf8f7c1edaebe06b6755d18311a5f0
krishna.gupta <krishna.gupta@gojek.com>
2021-05-23 14:24:05 +0530

    Add Solution Proposed By The Book

---
+++ chapter1/simuduck_booksolution/behaviours.go
@@ -0,0 +1,77 @@
+// Implementing the solution from the book of course in idiomatic go
+package main
+
+import "fmt"
+
+type FlyBehaviour interface {
+	Fly()
+}
+
+type DefaultFly struct {
+	Height int
+	Speed  int
+}
+
+func (df *DefaultFly) Fly() {}
+
+type RocketPoweredFlying struct {
+	Height         int
+	Speed          int
+	RocketCapacity int
+	FuelType       string
+}
+
+func (rpf *RocketPoweredFlying) validateCapacityAccordingToLaw() error {
+	if rpf.RocketCapacity > 1200 {
+		return fmt.Errorf("This country does not allow rocket capacity more than 1200cc")
+	}
+	return nil
+}
+
+// NewRocketPoweredFlying() is just an hyped example to demonstrate how the
+// solution from the book has capability to much more.
+// Doing some work before returning the object satisfying the interface
+func NewRocketPoweredFlying(height int, speed int, rocketcap int, fuel string) (FlyBehaviour, error) {
+	rpf := &RocketPoweredFlying{
+		Height:         height,
+		Speed:          speed,
+		RocketCapacity: rocketcap,
+		FuelType:       fuel,
+	}
+
+	if err := rpf.validateCapacityAccordingToLaw(); err != nil {
+		return nil, err
+	}
+	return rpf, nil
+}
+
+func (rpf *RocketPoweredFlying) Fly() {}
+
+type QuackBehaviour interface {
+	Quack()
+}
+
+type DefaultQuack struct {
+	Frequency int
+}
+
+func (dq *DefaultQuack) Quack() {}
+
+type MuteQuack struct{}
+
+func (mq *MuteQuack) Quack() {}
+
+type SwimBehaviour interface {
+	Swim()
+}
+
+type DefaultSwimBehaviour struct {
+	Speed int
+}
+func (dsb *DefaultSwimBehaviour) Swim(){}
+
+type UnderwaterSwimBehaviour struct {
+	Depth int
+	Speed int
+}
+func (uwsb *UnderwaterSwimBehaviour) Swim(){}
---
+++ chapter1/simuduck_booksolution/main.go
@@ -0,0 +1,59 @@
+// This package is an attempt to replicate the solution from the book
+package main
+
+import "fmt"
+
+// Duck is an attempt to port from the Duck class mentioned in the book
+//
+// Problem: Why does all Duck need to have FlyBehavior? even if it never going to
+// fly. Same with QuackBehavior.
+type Duck struct {
+	fb FlyBehaviour
+	qb QuackBehaviour
+	display func()
+}
+
+func (d *Duck) performFly(){
+	d.fb.Fly()
+}
+
+func (d *Duck) performQuack(){
+	d.fb.Fly()
+}
+
+func (d *Duck) swim(){
+	fmt.Println("All ducks float, even decoys!")
+}
+
+
+type MallardDuck struct {
+	Duck
+}
+
+// I don't think these setters are needed here, since instantiating
+// the struct gives you the option to set all the fields.
+func (md *MallardDuck) setFlyBehavior(fb FlyBehaviour){
+	md.fb = fb
+}
+
+func (md *MallardDuck) setQuackBehavior(qb QuackBehaviour){
+	md.qb = qb
+}
+
+
+func main() {
+	mfly, err := NewRocketPoweredFlying(1000, 100, 999, "ethanol")
+	if err != nil {
+		panic(err)
+	}
+	var md *MallardDuck = &MallardDuck{Duck{
+		fb:                     mfly,
+		qb:                     &DefaultQuack{},
+		display: func() {
+			fmt.Println("I am a mallard duck")
+		},
+	}}
+
+	md.performFly()
+	md.display()
+}
