598183d1747c63b9780f581251f50b7dad28e8fa
krishna.gupta <krishna.gupta@gojek.com>
2021-06-06 16:35:43 +0530

    Add Seccond Condiment to the POC
    Adding second condiment helped understand the reach of
    polymorphism on Cost() and Description() calls goes all the
    way back to the main DarkRoast Type

--- chapter3/starbuzz/main.go
+++ chapter3/starbuzz/main.go
@@ -9,6 +9,9 @@ type Beverage interface {
 	Cost() Dollar
 }
 
+// TODO: `price` and `cost` are two words used as same meaning...
+// TODO: ...Causing confusion
+
 // DarkRoast is an Beverage
 type DarkRoast struct {
 	basePrice   Dollar
@@ -34,11 +37,12 @@ type Condiment interface {
 	AddToBeverage(Beverage) Beverage
 }
 
-// Mocha is a Condiment and also a Beverage
+// Mocha is a Condiment but satisfies the Beverage interface too
 type Mocha struct {
 	basePrice   Dollar
 	description string
 
+	newDescription string
 	newPrice Dollar
 }
 
@@ -47,26 +51,68 @@ type Mocha struct {
 func (m *Mocha) AddToBeverage(beverage Beverage) Beverage {
 	// change the price
 	// change description
-	m.basePrice = 7.99
-	m.description = "Charged with One Mocha."
+	m.newDescription = fmt.Sprintf("%s %s", beverage.Description(), m.description)
 	m.newPrice = beverage.Cost() + m.basePrice
 	return m
 }
 
 func (m *Mocha) Description() string {
-	return m.description
+	return m.newDescription
 }
 
 func (m *Mocha) Cost() Dollar {
 	return m.newPrice
 }
 
+func NewMocha() Condiment {
+	return &Mocha{
+		basePrice:   7.99,
+		description: "Charged with One Mocha.",
+	}
+}
+
+// Whip is a Condiment but satisfies the Beverage interface too
+type Whip struct {
+	basePrice   Dollar
+	description string
+
+	newDescription string
+	newPrice Dollar
+}
+
+func (w *Whip) Description() string {
+	return w.newDescription
+}
+
+func (w *Whip) Cost() Dollar {
+	return w.newPrice
+}
+
+func (w *Whip) AddToBeverage(beverage Beverage) Beverage {
+	w.newDescription = fmt.Sprintf("%s %s", beverage.Description(), w.description)
+	w.newPrice = beverage.Cost() + w.basePrice
+	return w
+}
+
+func NewWhip() Condiment {
+	return &Whip{
+		basePrice:   7.99,
+		description: "Whipped, not whooped.",
+	}
+}
+
 func main() {
 	// trying wrapping / decorating
 	dr := NewDarkRoast()
 	fmt.Printf("Base Cost of your order:\n%f : %s\n", dr.Cost(), dr.Description())
+
 	// adding Mocha
-	m := Mocha{}
+	m := NewMocha()
 	dr = m.AddToBeverage(dr)
+	fmt.Printf("New Cost of your order:\n%f : %s\n", dr.Cost(), dr.Description())
+
+	// adding whip
+	w := NewWhip()
+	dr = w.AddToBeverage(dr)
 	fmt.Printf("New Cost of your order:\n%f : %s\n", dr.Cost(), dr.Description())
 }
